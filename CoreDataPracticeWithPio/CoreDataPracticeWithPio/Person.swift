418d4dc5615ee8e3700cdf8bb7e5aee0bc629ddf
kyungpyoda <qorghdl@naver.com>
2020-11-18 16:37:54 +0900

    Person 모델 생성
    - Contact Entity에 값을 주입시키기 위한 구조체임

---
+++ CoreDataPracticeWithPio/CoreDataPracticeWithPio/Person.swift
@@ -0,0 +1,14 @@
+//
+//  Person.swift
+//  CoreDataPracticeWithPio
+//
+//  Created by 홍경표 on 2020/11/18.
+//
+
+import Foundation
+
+struct Person {
+    var name: String
+    var phoneNumber: String
+    var shortcutNumber: Int
+}
