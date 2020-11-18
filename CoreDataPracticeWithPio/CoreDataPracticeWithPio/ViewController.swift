c6af9a98700b859b9ae49186e0b10ce00b0793ff
kyungpyoda <qorghdl@naver.com>
2020-11-18 16:45:00 +0900

    viewDidLoad시 데이터 생성하여 저장

--- CoreDataPracticeWithPio/CoreDataPracticeWithPio/ViewController.swift
+++ CoreDataPracticeWithPio/CoreDataPracticeWithPio/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    
+    var appDelegate: AppDelegate {
+        UIApplication.shared.delegate as! AppDelegate
+    }
+    
+    private let pio = Person(name: "Pio", phoneNumber: "010-0101-0101", shortcutNumber: 1)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Contact", in: context)
+        
+        if let entity = entity {
+            let person = NSManagedObject(entity: entity, insertInto: context)
+            person.setValue(pio.name, forKey: "name")
+            person.setValue(pio.phoneNumber, forKey: "phoneNumber")
+            person.setValue(pio.shortcutNumber, forKey: "shortcutNumber")
+        }
+        do {
+            try context.save()
+        } catch {
+            print(error.localizedDescription)
+        }
     }
-
-
 }
-
