9075828734db1d26cdc6a015092b2e9489fb9b3b
kyungpyoda <qorghdl@naver.com>
2020-11-18 15:31:58 +0900

    init Project

---
+++ CoreDataPracticeWithPio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataPracticeWithPio
+//
+//  Created by 홍경표 on 2020/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
